76004f267ac85591ca67e0831e7161f95cf93b58
MisaVz <ojomuertop@gmail.com>
2025-06-09 07:07:47 -0600

    AAAAH

---
+++ WebApiPatrimonio/Controllers/PantallasController.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using WebApiPatrimonio.Services;
+
+[ApiController]
+[Route("pantallas")]
+public class PantallasController : ControllerBase
+{
+    private readonly IConfiguration _cfg;
+
+    public PantallasController(IConfiguration cfg)
+    {
+        _cfg = cfg;
+    }
+
+    [HttpGet("sidebar")]
+    public IActionResult GetPantallasSidebar()
+    {
+        using var cnn = new SqlConnection(_cfg.GetConnectionString("Conexion"));
+
+        var pantallas = cnn.Query(
+            """
+            SELECT Pantalla AS Ruta,
+                   ISNULL(NombreMenu, Pantalla) AS Nombre
+            FROM   CAT_PANTALLAS_CFG
+            WHERE  Activo = 1 AND MostrarSidebar = 1
+            ORDER BY Orden;
+            """
+        );
+
+        return Ok(pantallas);
+    }
+}
